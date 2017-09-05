373245dde63e90f918073b990f25119ed441b276
Ankur Sinha (Ankur Sinha Gmail) <sanjay.ankur@gmail.com>
2017-09-05 10:10:42 +0100

    Correctly disable str p updates in nest

--- nestkernel/simulation_manager.cpp
+++ nestkernel/simulation_manager.cpp
@@ -662,35 +662,6 @@ nest::SimulationManager::update_()
         gettimeofday( &t_slice_begin_, NULL );
       }
 
-      if ( kernel().sp_manager.is_structural_plasticity_enabled()
-        && ( clock_.get_steps() + from_step_ )
-            % kernel().sp_manager.get_structural_plasticity_update_interval()
-          == 0 )
-      {
-        for ( std::vector< Node* >::const_iterator i =
-                kernel().node_manager.get_nodes_on_thread( thrd ).begin();
-              i != kernel().node_manager.get_nodes_on_thread( thrd ).end();
-              ++i )
-        {
-          ( *i )->update_synaptic_elements(
-            Time( Time::step( clock_.get_steps() + from_step_ ) ).get_ms() );
-        }
-#pragma omp barrier
-#pragma omp single
-        {
-          kernel().sp_manager.update_structural_plasticity();
-        }
-        // Remove 10% of the vacant elements
-        for ( std::vector< Node* >::const_iterator i =
-                kernel().node_manager.get_nodes_on_thread( thrd ).begin();
-              i != kernel().node_manager.get_nodes_on_thread( thrd ).end();
-              ++i )
-        {
-          ( *i )->decay_synaptic_elements_vacant();
-        }
-      }
-
-
       if ( from_step_ == 0 ) // deliver only at beginning of slice
       {
         kernel().event_delivery_manager.deliver_events( thrd );
@@ -889,6 +860,7 @@ nest::SimulationManager::update_()
     {
       ( *i )->update_synaptic_elements(
         Time( Time::step( clock_.get_steps() + to_step_ ) ).get_ms() );
+      ( *i )->decay_synaptic_elements_vacant();
     }
 
   } // end of #pragma parallel omp
